c776f4d773b94c0eedd3a6b5629c4b96b4aed39e
fabriciopk <bottegafabricio@gmail.com>
2020-07-19 12:51:03 -0300

    refactor: refactoring farm table using new farm and beehive model

--- server/web/app/__init__.py
+++ server/web/app/__init__.py
@@ -1,11 +1,14 @@
+import os
 import json
 import random
 import time
 from datetime import datetime
 
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, send_from_directory
 from importlib import import_module
-from influxdb import InfluxDBClient
+
+from app.model.beehive import BeeHive
+from app.model.farm import Farm
 
 application = Flask(__name__, static_folder='home/static')
 random.seed()
@@ -15,69 +18,18 @@ random.seed()
 def index():
     return render_template('index.html')
 
+@application.route('/beehive/<id>')
+def beehive(id):
+    return render_template('index.html')
 
-def influx_query(query, host='localhost', port=8086):
-    """
-    Execute influx db query
-    """
-    user = 'root'
-    password = 'root'
-    dbname = 'honeycomb'
-    client = InfluxDBClient(host, port, user, password, dbname)
-    return client.query(query)
-
-
-def all_reads():
-    """
-    Return all mesurements from a given node.
-    """
-    q = influx_query("select * from pot;")
-    return list(q.get_points())
-
-
-def select_entries(time_delta):
-    q = influx_query(f"SELECT * FROM pot WHERE time > now() - {time_delta}h")
-    return list(q.get_points())
-
-
-def last_info():
-    """
-    Returns last info added to db.
-    """
-    q = influx_query("SELECT * FROM pot GROUP BY * ORDER BY ASC LIMIT 1")
-    return list(q.get_points())
-
-
-def count_readings():
-    q = influx_query("SELECT COUNT(dht_status)  FROM pot;")
-    return list(q.get_points())[0]['count']
-
-
-@application.route('/count')
-def count():
-    return str(count_readings())
-
-
-@application.route('/last_info')
-def count_route():
-    return json.dumps(last_info())
-
-
-
-@application.route('/data')
-def data_route():
-    data = all_reads()
-    return json.dumps(data)
-
-
-@application.route('/data/<time_delta>')
-def entry_route(time_delta):
-    return json.dumps(select_entries(int(time_delta)))
+@application.route('/data/<id>')
+def data_route(id):
+    bh = BeeHive(id)
+    return json.dumps(bh.select_all())
 
 
 @application.route('/chart-data')
 def chart_data():
-    # data = influx_data()
     def generate_random_data():
         while True:
             json_data = json.dumps({})
@@ -85,6 +37,11 @@ def chart_data():
             time.sleep(1)
     return Response(generate_random_data(), mimetype='text/event-stream')
 
+
+@application.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(application.root_path, 'home/static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.ico')
 
 def create_app():
     module = import_module('app.home.routes')
--- server/web/app/home/routes.py
+++ server/web/app/home/routes.py
@@ -1,10 +1,12 @@
 from app.home import blueprint
 from flask import redirect, url_for, render_template
-
+from app.model.farm import Farm
 
 @blueprint.route('/<template>')
 def route_template(template):
     try:
+        if template == "ui-tables":
+            return render_template(template + '.html', farm=Farm)
         return render_template(template + '.html')
     except Exception:
         return render_template('page-404.html'), 404
