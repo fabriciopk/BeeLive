69dcae05ca0b614b35dcc808f05f9f39e2a83e3b
fabriciopk <bottegafabricio@gmail.com>
2020-07-19 12:48:42 -0300

    Remove database access logic from flask entrypoint

---
+++ server/web/app/db/influxdb.py
@@ -0,0 +1,14 @@
+from influxdb import InfluxDBClient
+
+def influx_query(query, host='localhost', port=8086):
+    """
+    Execute influx db query
+    """
+    user = 'root'
+    password = 'root'
+    dbname = 'honey-comb'
+    client = InfluxDBClient(host, port, user, password, dbname)
+    try:
+        return client.query(query)
+    except Exception as e:
+        print(f"Not possible to fetch data from db {e}.")
