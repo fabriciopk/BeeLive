c56c4b9e969723b79fa0e223eb55e48ee608c28e
fbottega <bottegafabricio@gmail.com>
2020-08-24 20:48:16 -0300

    Remove unused variables from esp32 project

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -33,7 +33,6 @@
 static const char *TAG = "HIVE_STATUS";
 static const char *WIFI_TAG = "WIFI";
 static const char *READING_TAG = "SENSORS";
-static const char *MQTT_TAG = "MQTT";
 
 #define DOUT_GPIO   18
 #define PD_SCK_GPIO 19
@@ -154,9 +153,6 @@ void read_sensors(void *params) {
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-    // your_context_t *context = event->context;
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
